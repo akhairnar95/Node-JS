f47bd96ba5e41fe12ec1fb570c1b6d07d915515c
akhairnar95 <105839156+akhairnar95@users.noreply.github.com>
2024-04-28 18:13:55 +0530

    Initial commit

---
+++ Backend/db/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: String,
+  category: String,
+  userId: String,
+  company: String,
+});
+
+const User = mongoose.model("products", productSchema);
+module.exports = User;
